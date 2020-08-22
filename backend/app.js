4d18c5431a7427fdbcbd2a78c8ea53553582ab6f
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-08-21 21:29:25 -0400

    ssl redirect

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var episodesRouter = require('./routes/episodes');
 
+import sslRedirect from "heroku-ssl-redirect";
+
 var app = express();
+
+app.use(sslRedirect());
 
 var cors = require("cors");
 app.use(cors());
