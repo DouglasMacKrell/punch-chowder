9950e26d1cbf91e9f9ed96f4b2ccf4ba848acef3
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-08-21 22:05:46 -0400

    redirect problems

--- backend/app.js
+++ backend/app.js
@@ -21,11 +21,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 // app.use(express.static(path.join(__dirname, "/public")));
 
-app.use(sslRedirect());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/episodes', episodesRouter);
+
+app.use(sslRedirect());
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
