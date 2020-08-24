7fcfb458b62d6cb91363ec929b683942641be499
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-08-24 09:27:23 -0400

    store link added to Secrets

--- frontend/src/Components/TopSecret/TopSecret.jsx
+++ frontend/src/Components/TopSecret/TopSecret.jsx
@@ -7,6 +7,7 @@ const TopSecret = () => {
             <div className="top-secret__main-container">
                 <div className="top-secret__sub-container">
                     <a href="https://douglasmackrell.com"><button>CREDITS</button></a>
+                    <a href="https://www.redbubble.com/people/DouglasMacKrell/shop?asc=u"><button>STORE</button></a>
                 </div>
             </div>
             
