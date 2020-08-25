22eedbf14b5333f160152738350d2aaf7c825068
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-08-25 00:05:19 -0400

    Secrets update with video host credits

--- frontend/src/components/TopSecret/TopSecret.jsx
+++ frontend/src/components/TopSecret/TopSecret.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import './TopSecret.css'
+import React from "react";
+import "./TopSecret.css";
 
 const TopSecret = () => {
-    return (
-        <div className="top-secret">
-            <div className="top-secret__main-container">
-                <div className="top-secret__sub-container">
-                    <a href="https://douglasmackrell.com"><button>CREDITS</button></a>
-                    <a href="https://www.redbubble.com/people/DouglasMacKrell/shop?asc=u"><button>STORE</button></a>
-                </div>
-            </div>
-            
+  return (
+    <div className="top-secret">
+      <div className="top-secret__main-container">
+        <div className="top-secret__sub-container">
+          <div className="top-secret__doug">
+              <h2>SITE CREATED BY</h2>
+            <a href="https://douglasmackrell.com">
+              <button>PORTFOLIO</button>
+            </a>
+            <a href="https://www.redbubble.com/people/DouglasMacKrell/shop?asc=u">
+              <button>STORE</button>
+            </a>
+          </div>
+          <div className="top-secret__video-owner">
+              <h2>VIDEO CLIPS HOSTED BY</h2>
+              <a href="https://twitter.com/tmntlovingleo"><button>TWITTER</button></a>
+              <a href="https://tmntlovingleocreates.tumblr.com/"><button>TUMBLR</button></a>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default TopSecret
+export default TopSecret;
